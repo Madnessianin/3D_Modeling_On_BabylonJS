13a1998b788d94be319d26dd25b08a79b3f93d2f
Madnessianin <madnessstalkerl@gmail.com>
2021-03-27 18:44:32 +0500

    add function create connections

--- src/Redux/mineReducer.ts
+++ src/Redux/mineReducer.ts
@@ -1,6 +1,78 @@
 const SET_DATA: string = "3dmodelingBabylonJS/SET_DATA",
   SET_SCALE_MINE: string = "3dmodelingBabylonJS/SET_SCALE_MINE";
 
+type elemType = {
+  id: string;
+  point: {
+    x: number;
+    y: number;
+    z: number;
+  };
+  linkedNodes: Array<string>;
+};
+
+const createConnection = (array: Array<elemType>): Array<{}> => {
+  type arrayPoint = {
+    [key: string]: {
+      x: number;
+      y: number;
+      z: number;
+    };
+  };
+  let arrayPoint: arrayPoint = {};
+  const arraysLinks: Array<Array<string>> = [];
+  array.forEach((item) => {
+    const point = { [item.id]: item.point };
+    arrayPoint = {
+      ...arrayPoint,
+      ...point,
+    };
+    arraysLinks.push([...item.linkedNodes]);
+  });
+  const arrayId: Array<string> = Object.keys(arrayPoint);
+
+  const arrayNodeId: Array<string> = [];
+
+  type connectElemType = {
+    id: string;
+    point: {
+      x: number;
+      y: number;
+      z: number;
+    };
+  };
+
+  type connectType = {
+    pointOne: connectElemType;
+    pointTwo: connectElemType;
+  };
+
+  const connections: Array<connectType> = [];
+
+  arraysLinks.forEach((item, index) => {
+    arrayNodeId.push(arrayId[index]);
+    item.forEach((elem) => {
+      if (arrayNodeId.indexOf(elem) === -1) {
+        connections.push({
+          pointOne: {
+            id: arrayId[index],
+            point: {
+              ...arrayPoint[arrayId[index]],
+            },
+          },
+          pointTwo: {
+            id: elem,
+            point: {
+              ...arrayPoint[elem],
+            },
+          },
+        });
+      }
+    });
+  });
+  return connections;
+};
+
 const initialState: Object = {
   id: "",
   title: "",
@@ -12,6 +84,7 @@ const initialState: Object = {
     scaleY: 1,
     scaleZ: 1,
   },
+  connections: [],
 };
 
 type stateType = typeof initialState;
